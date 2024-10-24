410768ebb29c2808ec648d6271afeefc16402693
MassimoCorvo <massimocorvo22@gmail.com>
2024-10-24 14:18:20 +0200

    score, hight score, try again

--- main.js
+++ main.js
@@ -1,34 +1,84 @@
 const myForm = document.querySelector("form");
 const formNumber = document.getElementById("number-choice");
+const again = document.getElementById("again");
+const printScore = document.getElementById("print-score");
+const checkButton = document.getElementById("check-button");
+const result = document.getElementById("result");
+const guessing = document.getElementById("start-guessing");
+
+let numberToGuess = getRndInteger(1, 20);
+
+let score = 20;
+let highScore = 0;
 
 myForm.addEventListener("submit", function (event) {
-    
-    document.querySelector("body").classList.remove("bg-success");
-    document.getElementById("printResult").innerText = "Guess my number";
 
     event.preventDefault();
 
     console.log(formNumber.value);
 
-    let numberToGuess = getRndInteger(1, 20);
-
     if (formNumber.value == numberToGuess) {
-        //Cambia background in verde
-        document.querySelector("body").classList.add("bg-success");
+        if (score > 0) {
+            score--;
 
-        //Mostra il numero indovinato
-        document.getElementById("result").innerText = numberToGuess;
-        document.getElementById("printResult").innerText = "Hai vinto";
+            //Update score
+            printScore.innerText = score;
+            
+            //Cambia background in verde
+            document.querySelector("body").classList.add("bg-success");
+
+            //Mostra il numero indovinato
+            result.innerText = numberToGuess;
+            guessing.innerText = "Hai vinto";
+        }
+
+        checkButton.setAttribute("disabled", "");
+
+        if (highScore < score) {
+            highScore = score;
+            document.getElementById("print-high-score").innerText = highScore;
+        }
     }
 
-    else{
-        document.getElementById("printResult").innerText = "Riprova";
+    else {
+        if (score > 0) {
+
+            if( formNumber.value < numberToGuess )
+                guessing.innerText = "Too low";
+            else{
+                guessing.innerText = "Too high";
+            }
+            score--;
+            printScore.innerText = score;
+        }
+
+        else {
+            guessing.innerText = "Hai perso";
+        }
     }
+
+})
+
+//Reset
+again.addEventListener("click", function () {
+
+    document.querySelector("body").classList.remove("bg-success");
+    checkButton.removeAttribute("disabled");
+    document.getElementById("start-guessing").innerText = "Start guessing...";
+
+    //Nasconde il numero da indovinare
+    result.innerText = "???";
+
+    //Refresh score
+    score = 20;
+    printScore.innerText = score;
+
+    //Nuovo numero random
+    numberToGuess = getRndInteger(1, 20);
 
     formNumber.value = null;
 
 })
-
 
 //FUNCTIONS
 
